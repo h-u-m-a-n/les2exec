aa220d85c9c5382de366a86d958d7f6611d5226b
Sanzhar <190103205@stu.sdu.edu.kz>
2021-10-24 16:54:18 +0600

    change letter case func used

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/h-u-m-a-n/les2exec
+
+go 1.16
+
+require github.com/h-u-m-a-n/les2lib v1.0.0
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/h-u-m-a-n/les2lib"
+)
+
+func main() {
+	// 1.0.0
+	fmt.Printf("%c\n", les2lib.ChangeLetterCase('a'))
+	fmt.Printf("%c\n", les2lib.ChangeLetterCase('B'))
+	fmt.Printf("%c\n", les2lib.ChangeLetterCase('-'))
+
+}
