e21b6c1afae6d4f0b93e36bc148eaabce31c3062
Sanzhar <190103205@stu.sdu.edu.kz>
2021-10-24 17:03:14 +0600

    find roots func used

--- main.go
+++ main.go
@@ -11,4 +11,20 @@ func main() {
 	fmt.Printf("%c\n", les2lib.ChangeLetterCase('B'))
 	fmt.Printf("%c\n", les2lib.ChangeLetterCase('-'))
 
+	// 1.0.1
+	r1, r2, ok := les2lib.FindRoots(3, 2,1)
+	showFindRootsResult(r1,r2,ok)
+	r1,r2,ok = les2lib.FindRoots(3,2,-1)
+	showFindRootsResult(r1,r2,ok)
+	r1,r2,ok = les2lib.FindRoots(9,-12,4)
+	showFindRootsResult(r1,r2,ok)
+
+}
+
+func showFindRootsResult(r1, r2 float64, ok error) {
+	if ok != nil {
+		fmt.Println(ok)
+	} else {
+		fmt.Println(r1, r2)
+	}
 }
